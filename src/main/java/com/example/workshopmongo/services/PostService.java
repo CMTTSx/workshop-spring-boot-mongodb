225daa9f18cc44ece6fe886e87f58dca48ecb832
Calil Sousa Mattos <contatocsmpm@gmail.com>
2023-10-14 17:22:47 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Optional<Post> findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        if (user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
+    }
+
+}
